2dd8ec605fbad80dd038de7ccac94b8cb542a715
Kevin <camorobot@gmail.com>
2023-03-26 23:33:11 +0200

    title screeb

--- src/main/java/nl/camorobot/stormbird/scenes/TitleScene.java
+++ src/main/java/nl/camorobot/stormbird/scenes/TitleScene.java
@@ -7,9 +7,15 @@ import nl.camorobot.stormbird.assets.buttons.PlayButton;
 import nl.camorobot.stormbird.assets.imageEntitys.StormBirdImg;
 import nl.camorobot.stormbird.birds.TitleSceneBird;
 
+import java.util.Random;
+
 public class TitleScene extends DynamicScene {
 
     private Stormbird stormbird;
+    int birdColor;
+    String sprite;
+    Random randBirdSprite = new Random();
+
 
     public TitleScene(Stormbird stormbird){
         this.stormbird = stormbird;
@@ -22,8 +28,21 @@ public class TitleScene extends DynamicScene {
 
     @Override
     public void setupEntities() {
+        for(int i = 0; i < 5; i++){
+            birdColor = i;
+            if(birdColor == 3){
+                birdColor = randBirdSprite.nextInt(0,2);
+            }
+            if(birdColor == 0){
+                sprite = "sprites/yellowBird-upflap.png";
+            } else if (birdColor == 1){
+                sprite = "sprites/blueBird-upflap.png";
+            } else if (birdColor == 2) {
+                sprite = "sprites/redBird-upflap.png";
+            }
+            addEntity(new TitleSceneBird(sprite, new Coordinate2D(getWidth()/2, getHeight()/2 + 50)));
+        }
         addEntity(new StormBirdImg());
         addEntity(new PlayButton(new Coordinate2D(getWidth()/2, getHeight()/2 - 50)));
-        addEntity(new TitleSceneBird(new Coordinate2D(getWidth()/2, getHeight()/2 + 50)));
     }
 }
