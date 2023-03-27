07b928fedee9285f729523301262778728ffdee1
Kevin <camorobot@gmail.com>
2023-03-27 19:48:14 +0200

    implement tubes

---
+++ src/main/java/nl/camorobot/stormbird/objects/Tube.java
@@ -0,0 +1,56 @@
+package nl.camorobot.stormbird.objects;
+
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
+
+import java.util.*;
+
+public class Tube extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Collider {
+
+    private double width;
+    private double height;
+
+    public int tubeNumber;
+
+    private String direction;
+    private Random randTube = new Random();
+
+    private List<Integer> topTubes = Arrays.asList(-250, -100, 0, 100);
+    private List<Integer> bottomTubes = Arrays.asList(650, 800, 900, 1000);
+
+
+    public Tube(String sprite, String direction, double width, double height) {
+        super(sprite, new Coordinate2D(width, height));
+        this.width = width;
+        this.height = height;
+        this.direction = direction;
+        setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        setMotion(6, 270d);
+    }
+
+    @Override
+    public void notifyBoundaryCrossing(SceneBorder sceneBorder) {
+        if(direction == "top"){
+            setAnchorLocation(new Coordinate2D(width + 50, topTubes.get(getTubeNumber())));
+        } else{
+            setAnchorLocation(new Coordinate2D(width+ 50, bottomTubes.get(getTubeNumber())));
+        }
+    }
+
+    public int getRandomTube(){
+        return randTube.nextInt(0,3);
+    }
+
+    public int getTubeNumber() {
+        return tubeNumber;
+    }
+
+    public void setTubeNumber(int tubeNumber) {
+        this.tubeNumber = tubeNumber;
+    }
+}
---
+++ src/main/java/nl/camorobot/stormbird/objects/coins/SilverCoin.java
@@ -0,0 +1,12 @@
+package nl.camorobot.stormbird.objects.coins;
+
+public class SilverCoin implements Coin{
+
+    public SilverCoin(){
+    }
+
+    @Override
+    public void incrementCoins() {
+
+    }
+}
