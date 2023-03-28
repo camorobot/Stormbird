32aff2cd37d9b3eac53f2b4afe28fa1ad69ffd0b
Kevin <camorobot@gmail.com>
2023-03-28 11:19:07 +0200

    setup database connection

--- src/main/java/nl/camorobot/stormbird/player/Player.java
+++ src/main/java/nl/camorobot/stormbird/player/Player.java
@@ -4,7 +4,11 @@ import nl.camorobot.stormbird.Stormbird;
 
 public class Player {
     private Stormbird stormbird;
+
+    private int player_id;
+    private String username;
     private int coins;
+    private int highScore;
 
     public Player(Stormbird stormbird){
         this.stormbird = stormbird;
@@ -16,5 +20,29 @@ public class Player {
 
     public void setCoins(int coins) {
         this.coins = coins;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(int highScore) {
+        this.highScore = highScore;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
     }
 }
