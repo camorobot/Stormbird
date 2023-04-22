6cb353b7ab775246a35066b5062f0eab253d28dd
Kevin <camorobot@gmail.com>
2023-04-22 13:54:40 +0200

    Get data from databse

--- src/main/java/nl/camorobot/stormbird/assets/buttons/PlayerButton.java
+++ src/main/java/nl/camorobot/stormbird/assets/buttons/PlayerButton.java
@@ -6,40 +6,31 @@ import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.impl.SpriteEntity;
 import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
 import javafx.scene.input.MouseButton;
-import nl.camorobot.stormbird.assets.text.PlayerUsernameText;
-import nl.camorobot.stormbird.scenes.TitleScene;
-
-import java.util.Objects;
+import nl.camorobot.stormbird.assets.text.CurrentPlayerText;
+import nl.camorobot.stormbird.dao.DatabaseDAO;
+import nl.camorobot.stormbird.dao.PlayerDAO;
+import nl.camorobot.stormbird.player.Player;
 
 public class PlayerButton extends SpriteEntity implements MouseButtonPressedListener {
-    private PlayerUsernameText _playerUsernameText;
-    private TitleScene _titileScene;
+    private CurrentPlayerText _playerUsernameText;
+    private String _username;
+    private String _sprite;
+    private Player _player;
 
-    public PlayerButton(PlayerUsernameText _playerUsernameText, Coordinate2D coordinate2D, TitleScene _titileScene){
-        super("sprites/player.png", coordinate2D, new Size(100,100));
-        this._playerUsernameText = _playerUsernameText;
-        this._titileScene = _titileScene;
+    public PlayerButton(String sprite, Coordinate2D coordinate2D, String _username, Size size, Player _player){
+        super(sprite, coordinate2D, size);
+        this._sprite = sprite;
+        this._username = _username;
+        this._player = _player;
         setAnchorPoint(AnchorPoint.CENTER_CENTER);
     }
 
     @Override
     public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
-        if(Objects.equals(_playerUsernameText.getUsernameText(), "Kevin") || Objects.equals(_playerUsernameText.getUsernameText(), "Player: Kevin")){
-            if(Objects.equals(_playerUsernameText.getUsernameText(), "Kevin")) {
-                _playerUsernameText.setPlayerUsernameText("Player: Kevin",AnchorPoint.CENTER_RIGHT);
-                _titileScene.setActivePlayer("Kevin");
-            } else {
-                _playerUsernameText.setPlayerUsernameText("Kevin",AnchorPoint.CENTER_LEFT);
-                _titileScene.setActivePlayer(null);
-            }
-        } else{
-            if(Objects.equals(_playerUsernameText.getUsernameText(), "Stormy")){
-                _playerUsernameText.setPlayerUsernameText("Player: Stormy",AnchorPoint.CENTER_RIGHT);
-                _titileScene.setActivePlayer("Stormy");
-            } else {
-                _playerUsernameText.setPlayerUsernameText("Stormy",AnchorPoint.CENTER_LEFT);
-                _titileScene.setActivePlayer(null);
-            }
-        }
+        _player.setUsername(_username);
+        DatabaseDAO _databaseDAO = new DatabaseDAO();
+        PlayerDAO playerDAO = new PlayerDAO(_player);
+        playerDAO.retrievePlayer(_databaseDAO.connect(), _username);
+        System.out.println(_player.toString());
     }
 }
