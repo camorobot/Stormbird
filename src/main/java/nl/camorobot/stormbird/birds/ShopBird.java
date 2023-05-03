6e91be9157f0be3f0a763731f0389806bafe9ea6
Kevin <camorobot@gmail.com>
2023-05-03 09:17:27 +0200

    select the bird on buying

--- src/main/java/nl/camorobot/stormbird/birds/ShopBird.java
+++ src/main/java/nl/camorobot/stormbird/birds/ShopBird.java
@@ -5,23 +5,28 @@ import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.impl.SpriteEntity;
 import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
 import javafx.scene.input.MouseButton;
+import nl.camorobot.stormbird.Stormbird;
 import nl.camorobot.stormbird.dao.DatabaseDAO;
 import nl.camorobot.stormbird.dao.PlayerDAO;
 import nl.camorobot.stormbird.player.Player;
 import nl.camorobot.stormbird.scenes.ShopScene;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class ShopBird extends SpriteEntity implements MouseButtonPressedListener {
 
+    private Stormbird _stormbird;
     private int _price;
     private String _sprite;
     private Player _player;
     private ShopScene _shopScene;
+    private ArrayList<Boolean> birdsSprites = new ArrayList<>();
 
-    public ShopBird(String sprite, Coordinate2D initialLocation, Integer price, Player player, ShopScene shopScene) {
+    public ShopBird(Stormbird stormbird, String sprite, Coordinate2D initialLocation, Integer price, Player player, ShopScene shopScene) {
         super(sprite, initialLocation, new Size(60,60));
+        this._stormbird = stormbird;
         this._sprite = sprite;
         this._price = price;
         this._player = player;
@@ -30,14 +35,17 @@ public class ShopBird extends SpriteEntity implements MouseButtonPressedListener
 
     @Override
     public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
-        System.out.println(_sprite);
         // kijk hier of je de bird al hebt, niet? dan buyBird() wel? dan moet je selectBird()
         // kunnen we ophalen met een _shopscene.getUnlocked()
-
-        try {
-            buyBird(_sprite, _player.getPlayer_id());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if(_shopScene.getBirdsSprites().contains(_sprite)){
+            //bird is unlocked
+            _stormbird.set_playerBird(new PlayerBird(_stormbird, _sprite, new Coordinate2D(getWidth(), getHeight())));
+        } else{
+            try {
+                buyBird(_sprite, _player.getPlayer_id());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -66,6 +74,7 @@ public class ShopBird extends SpriteEntity implements MouseButtonPressedListener
             DatabaseDAO databaseDAO = new DatabaseDAO();
             playerDAO.buyBird(databaseDAO.connect(), player_id, bird_id, _player.getCoins() - 100);
             _player.setCoins(_player.getCoins() - 100);
+            _stormbird.set_playerBird(new PlayerBird(_stormbird, _sprite, new Coordinate2D(getWidth(), getHeight())));
             System.out.println("Buying bird successful");
         }
 
