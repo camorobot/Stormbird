8923cdb4d0f2904f0a79ae657fef56699bd0f593
Kevin <camorobot@gmail.com>
2023-05-04 12:28:22 +0200

    implement speed

--- src/main/java/nl/camorobot/stormbird/assets/text/ScoreText.java
+++ src/main/java/nl/camorobot/stormbird/assets/text/ScoreText.java
@@ -1,26 +1,50 @@
 package nl.camorobot.stormbird.assets.text;
 
 import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.entities.impl.TextEntity;
+import com.github.hanyaeger.api.entities.impl.DynamicTextEntity;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import nl.camorobot.stormbird.observers.ScoreObserver;
 
-public class ScoreText extends TextEntity {
-    private int score = -1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreText extends DynamicTextEntity {
+    private int score;
+    private List<ScoreObserver> observers;
 
     public ScoreText(Coordinate2D initialLocation) {
         super(initialLocation);
-        setFont(Font.font("Roboto", FontWeight.NORMAL, 30));
-        setFill(Color.BLACK);
+        setFill(Color.WHITE);
+        setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        score = 0;
+        setText("Score: " + score);
+        observers = new ArrayList<>();
     }
 
-    public void setScore(int score){
-        this.score = score;
-        setText(String.valueOf(score));
-    }
-
-    public int getScore(){
+    public int getScore() {
         return score;
     }
+
+    public void setScore(int score) {
+        this.score = score;
+        setText("Score: " + score);
+        notifyObservers();
+    }
+
+    public void addObserver(ScoreObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(ScoreObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (ScoreObserver observer : observers) {
+            observer.onScoreUpdate(score);
+        }
+    }
 }
+
