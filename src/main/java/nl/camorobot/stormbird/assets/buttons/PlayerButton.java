e44f23f512dd60e022fce492363e179fb97ffc60
Kevin <camorobot@gmail.com>
2023-05-04 21:01:43 +0200

    update kies speler

--- src/main/java/nl/camorobot/stormbird/assets/buttons/PlayerButton.java
+++ src/main/java/nl/camorobot/stormbird/assets/buttons/PlayerButton.java
@@ -10,18 +10,21 @@ import nl.camorobot.stormbird.assets.text.CurrentPlayerText;
 import nl.camorobot.stormbird.dao.DatabaseDAO;
 import nl.camorobot.stormbird.dao.PlayerDAO;
 import nl.camorobot.stormbird.player.Player;
+import nl.camorobot.stormbird.scenes.TitleScene;
 
 public class PlayerButton extends SpriteEntity implements MouseButtonPressedListener {
     private CurrentPlayerText _playerUsernameText;
+    private TitleScene titleScene;
     private String _username;
     private String _sprite;
     private Player _player;
 
-    public PlayerButton(String sprite, Coordinate2D coordinate2D, String _username, Size size, Player _player){
+    public PlayerButton(String sprite, Coordinate2D coordinate2D, String _username, Size size, Player _player, TitleScene titleScene) {
         super(sprite, coordinate2D, size);
         this._sprite = sprite;
         this._username = _username;
         this._player = _player;
+        this.titleScene = titleScene;
         setAnchorPoint(AnchorPoint.CENTER_CENTER);
     }
 
@@ -32,5 +35,7 @@ public class PlayerButton extends SpriteEntity implements MouseButtonPressedList
         PlayerDAO playerDAO = new PlayerDAO(_player);
         playerDAO.retrievePlayer(_databaseDAO.connect(), _username);
         System.out.println(_player.toString());
+
+        titleScene.updateSelectedTextPosition(new Coordinate2D(getAnchorLocation().getX(), getAnchorLocation().getY() + getHeight() / 2 + 10));
     }
 }
--- src/main/java/nl/camorobot/stormbird/birds/PlayerBird.java
+++ src/main/java/nl/camorobot/stormbird/birds/PlayerBird.java
@@ -9,19 +9,19 @@ import com.github.hanyaeger.api.entities.Newtonian;
 import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
-import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
 import nl.camorobot.stormbird.Stormbird;
-import nl.camorobot.stormbird.assets.text.ScoreText;
 import nl.camorobot.stormbird.objects.coins.Coin;
 import nl.camorobot.stormbird.objects.coins.CoinInterface;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class PlayerBird extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, KeyListener, Newtonian, Bird, Collided {
+public class PlayerBird extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Newtonian, Bird, Collided {
 
     private Stormbird stormbird;
+    private Set<KeyCode> pressedKeys = new HashSet<>();
 
     public PlayerBird(Stormbird stormbird, String sprite, Coordinate2D initialLocation) {
         super(sprite, initialLocation, new Size(60,60));
@@ -36,12 +36,10 @@ public class PlayerBird extends DynamicSpriteEntity implements SceneBorderCrossi
         setMotion(8, 180d);
     }
 
-    @Override
-    public void onPressedKeysChange(Set<KeyCode> set) {
-        if(set.contains(KeyCode.SPACE)){
+    public void handleKeys(Set<KeyCode> keys) {
+        pressedKeys = keys;
+        if (pressedKeys.contains(KeyCode.SPACE)) {
             jump();
-        } else if (set == null) {
-            System.out.println("oeps!");
         }
     }
 
